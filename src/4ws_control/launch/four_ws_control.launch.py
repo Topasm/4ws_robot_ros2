1d52c8bb88db5a61a9ad8672425a3082514d0775
Topasm <dhkdwk0215@naver.com>
2023-08-15 11:14:43 +0900

    updae

--- src/4ws_control/launch/four_ws_control.launch.py
+++ src/4ws_control/launch/four_ws_control.launch.py
@@ -1,10 +1,63 @@
+#!/usr/bin/env python3
+import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+
+
+import xacro
+
 def generate_launch_description():
 
+    robot_name = "4ws"
+    package_name = robot_name + "_description"
+    robot_description = os.path.join(get_package_share_directory(
+    package_name), "urdf", robot_name + ".urdf.xacro")
+    robot_description_config = xacro.process_file(robot_description)
+
+    controller_config = os.path.join(
+        get_package_share_directory(
+            package_name), "config", "controllers.yaml"
+    )
+
     return LaunchDescription([
+
+
+        # Node(
+        #     package="controller_manager",
+        #     executable="spawner",
+        #     arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        #     output="screen",
+        # ),
+
+        # Node(
+        #     package="controller_manager",
+        #     executable="spawner",
+        #     arguments=["velocity_controller", "-c", "/controller_manager"],
+        #     output="screen",
+        # ),
+
+        # Node(
+        #     package="controller_manager",
+        #     executable="spawner",
+        #     arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+        #     output="screen",
+        # ),
+
+        # Node(
+        #     package="robot_state_publisher",
+        #     executable="robot_state_publisher",
+        #     name="robot_state_publisher",
+        #     parameters=[
+        #         {"robot_description": robot_description_config.toxml()}],
+        #     output="screen",
+        # ),
+
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
